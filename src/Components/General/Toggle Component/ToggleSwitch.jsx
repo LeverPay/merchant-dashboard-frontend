3024c2747857e2484a6b79ab976e1427fe2de4ad
fusion <fusionboltinc@gmail.com>
2023-03-24 11:10:01 +0100

    SidebarComponents
    Signed-off-by: fusion <fusionboltinc@gmail.com>

--- src/Components/General/Toggle Component/ToggleSwitch.jsx
+++ src/Components/General/Toggle Component/ToggleSwitch.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
-import Switch from "@mui/joy/Switch";
+import React, {useState} from "react";
+import {Switch} from "@mui/joy";
 
 export default function ToggleSwitch() {
-  const [checked, setChecked] = useState(false);
-
-  const handleChange = () => {
-    setChecked((prev) => !prev);
-  };
-
-  return (
-    <div>
-      <Switch checked={checked} onChange={handleChange} />
-    </div>
-  );
+	const [checked, setChecked] = useState(false);
+	
+	const handleChange = () => {
+		setChecked((prev) => !prev);
+	};
+	
+	return (
+		<div>
+			<Switch checked={checked} onChange={handleChange}/>
+		</div>
+	);
 }
