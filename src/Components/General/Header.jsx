2ecfb566a527d54bf8159ff4764e6096fb57d675
DaScOrPiO <dunni82@yahoo.com>
2023-04-07 16:50:39 +0100

    FEAT: Problem on activelinks solved, pages named according to route

--- src/Components/General/Header.jsx
+++ src/Components/General/Header.jsx
@@ -1,10 +1,15 @@
 import React, { useLayoutEffect, useRef, useState } from "react";
 import TopNav from "./Header-components/TopNav";
 import SidebarNav from "./SidebarNav";
+import { useLocation } from "react-router-dom";
 
 export default function Header() {
   const ref = useRef(null);
   const [height, setHeight] = useState(0);
+  const location = useLocation();
+  console.log(location.pathname);
+  let name = location.pathname;
+  const pageName = name.replace(/^./, "").slice(0).toUpperCase();
 
   useLayoutEffect(() => {
     setHeight(ref.current.offsetHeight);
@@ -15,7 +20,7 @@ export default function Header() {
       <nav className="top-nav" ref={ref}>
         <div className="d-flex flex-column justify-content-between border-bottom shadow-sm px-4 py-3">
           <TopNav />
-          <div className="fw-semibold fs-5">Transaction</div>
+          <div className="fw-semibold fs-5">{pageName}</div>
         </div>
       </nav>
 
