b9e70dfcd784ab946e781f6bd2c739bb73728379
DaScOrPiO <dunni82@yahoo.com>
2023-04-14 17:48:53 +0100

    FEAT: Page named Dynamically according to page route

--- src/Components/General/Header.jsx
+++ src/Components/General/Header.jsx
@@ -7,9 +7,15 @@ export default function Header() {
   const ref = useRef(null);
   const [height, setHeight] = useState(0);
   const location = useLocation();
-  console.log(location.pathname);
   let name = location.pathname;
-  const pageName = name.replace(/^./, "").slice(0).toUpperCase();
+  let firstchar = name.replace(/^./, "").slice(0, 1).toUpperCase();
+  let pageName = firstchar.concat(name.replace(/^./, "").slice(1));
+  let symbol = pageName.indexOf("_") + 1;
+  const replace = pageName.charAt(symbol).toUpperCase();
+  // console.log(pageName.split("").splice(symbol, 0, replace));
+  const arr = pageName.split("");
+  arr.splice(symbol, 1, replace);
+  const replaced = arr.join("");
 
   useLayoutEffect(() => {
     setHeight(ref.current.offsetHeight);
@@ -20,7 +26,22 @@ export default function Header() {
       <nav className="top-nav" ref={ref}>
         <div className="d-flex flex-column justify-content-between border-bottom shadow-sm px-4 py-3">
           <TopNav />
-          <div className="fw-semibold fs-5">{pageName}</div>
+          <div
+            className={`fw-semibold fs-5 ${
+              pageName === `Profile` ||
+              pageName === `Transactions` ||
+              pageName === `Security` ||
+              (pageName === `Payment_method` && symbol !== -1)
+                ? `text-left`
+                : `text-center`
+            }`}
+          >
+            {pageName === ""
+              ? (pageName = "Overview")
+              : pageName.includes("_")
+              ? replaced.replace("_", " ")
+              : pageName}
+          </div>
         </div>
       </nav>
 
