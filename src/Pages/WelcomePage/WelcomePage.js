19a607354a92ca551a35b5218b439b8d242c4de2
Sarah <sarahnnanyelugo@gmail.com>
2023-05-25 14:08:34 +0100

    modification

--- src/Pages/WelcomePage/WelcomePage.js
+++ src/Pages/WelcomePage/WelcomePage.js
@@ -1,7 +1,13 @@
 import React from "react";
 import "./welcome-page.scss";
 function WelcomePage() {
-  return <div>WelcomePage</div>;
+  return (
+    <div className="col-md-10 offset-md-1 flexy welcome-page-container">
+      <div className="col-md-6">
+        <h1>Welcome!</h1>
+      </div>
+    </div>
+  );
 }
 
 export default WelcomePage;
