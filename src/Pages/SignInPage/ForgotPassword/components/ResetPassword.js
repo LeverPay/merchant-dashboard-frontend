d8cf1610796a4669bd15388947e0919db08e7f3e
DaScOrPiO <dunni82@yahoo.com>
2023-07-04 15:03:24 +0100

    FEAT: Added Reset password fields

---
+++ src/Pages/SignInPage/ForgotPassword/components/ResetPassword.js
@@ -0,0 +1,75 @@
+import React, { useState, useContext } from "react";
+import TokenContext from "../../../../Components/User-Token/TokenContext";
+import { Reset_password, baseUrl } from "../../../../Components/Endpoints";
+
+export default function ResetPassword() {
+  const { notify, success } = useContext(TokenContext);
+  const [Input, setInput] = useState({
+    token: "",
+    newpassword: "",
+    repeatPassword: "",
+  });
+  const passwordRegex =
+    /^(?=.*[!@#$%^&*()\-_=+{};:,<.>?])(?=.*[0-9])[a-zA-Z0-9!@#$%^&*()\-_=+{};:,<.>?]{10,}$/;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setInput((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (
+      Input.token !== "" &&
+      Input.newpassword !== "" &&
+      Input.repeatPassword === Input.newpassword
+    ) {
+      if (
+        passwordRegex.test(Input.newpassword) &&
+        passwordRegex.test(Input.repeatPassword)
+      ) {
+        success("valid ✔");
+        console.log(Input);
+      } else {
+        notify(
+          "Invalid Format: password field must be 10characters long and must include a special character"
+        );
+      }
+    } else {
+      notify("Invalid or Incomplete fields");
+    }
+  };
+
+  return (
+    <center>
+      <h1>Reset Password</h1>
+      <div className="col-md-3 forget-password-form">
+        <form onSubmit={handleSubmit}>
+          <h6>Token</h6>
+          <input
+            type="number"
+            onChange={handleChange}
+            value={Input.token}
+            name="token"
+          />
+          <h6>New Password</h6>
+          <input
+            type="password"
+            onChange={handleChange}
+            value={Input.newpassword}
+            name="newpassword"
+          />
+          <h6>Confirm Password</h6>
+          <input
+            type="password"
+            onChange={handleChange}
+            value={Input.repeatPassword}
+            name="repeatPassword"
+          />
+          <button>Reset Password</button>
+        </form>
+      </div>
+    </center>
+  );
+}
