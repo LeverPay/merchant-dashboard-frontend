76f61a2cb16f43b4f6df56f49d15ccdee72d0d75
Patrick <oludarepatrick@gmail.com>
2024-04-24 17:30:02 +0100

    Adjusted revenue page

--- src/Pages/AffliatePage/Merchant-revenue-Overview.jsx
+++ src/Pages/AffliatePage/Merchant-revenue-Overview.jsx
@@ -66,32 +66,32 @@ export default function MerchantRevenueOverview() {
                     <tbody className="table-body">
                     <tr>
                         <td className="table-text color-zeroB">23/10/23 10:12</td>
-                        <td className="table-text table-color-four">$10,000</td>
-                        <td className="table-text table-color-five">$320</td>
-                        <td className="table-text table-color-six">$1,200</td>
+                        <td className="table-text table-color-four">00.00</td>
+                        <td className="table-text table-color-five">00.00</td>
+                        <td className="table-text table-color-six">00.00</td>
                     </tr>
                     <tr>
                         <td className="table-text color-zeroB">23/10/23 10:12</td>
-                        <td className="table-text table-color-four">$5,000</td>
-                        <td className="table-text table-color-five">$9,300</td>
-                        <td className="table-text table-color-six">$4,200</td>
+                        <td className="table-text table-color-four">00.00</td>
+                        <td className="table-text table-color-five">00.00</td>
+                        <td className="table-text table-color-six">00.00</td>
                     </tr>
                     <tr>
                          <td className="table-text color-zeroB">23/10/23 10:12</td>
-                         <td className="table-text table-color-four">$50,000</td>
-                        <td className="table-text table-color-five">$32,220</td>
-                        <td className="table-text table-color-six">$200</td>
+                         <td className="table-text table-color-four">00.00</td>
+                        <td className="table-text table-color-five">00.00</td>
+                        <td className="table-text table-color-six">00.00</td>
                     </tr>
                     <tr>
                          <td className="table-text color-zeroB">23/10/23 10:12</td>
-                         <td className="table-text table-color-four">$50,000</td>
-                        <td className="table-text table-color-five">$32,220</td>
-                        <td className="table-text table-color-six">$200</td>
+                         <td className="table-text table-color-four">00.00</td>
+                        <td className="table-text table-color-five">00.00</td>
+                        <td className="table-text table-color-six">00.00</td>
                     </tr>                    <tr>
                          <td className="table-text color-zeroB">23/10/23 10:12</td>
-                         <td className="table-text table-color-four">$50,000</td>
-                        <td className="table-text table-color-five">$32,220</td>
-                        <td className="table-text table-color-six">$200</td>
+                         <td className="table-text table-color-four">00.00</td>
+                        <td className="table-text table-color-five">00.00</td>
+                        <td className="table-text table-color-six">00.00</td>
                     </tr>
                     
                     </tbody>
